85367d373027fab8c3cf865e33b44ac02c2f6152
sateesh <sateeshch206@gmail.com>
2023-12-21 10:46:46 +0530

    changes

--- src/components/Checkout/index.js
+++ src/components/Checkout/index.js
@@ -21,12 +21,19 @@ const Checkout = () => {
         )
     }
 
+     // Calculate the grand total
+     //the reduce function is used to iterate over the checkoutArr array and calculate the sum of the product of quantity and price for each item
+    const grandTotal = cartDetailsFromContext.checkoutArr.reduce(
+     (total, item) => total + item.quantity * item.price,
+    0
+  );
+
     return (
         <div className='checkout-page__bg-container'>
             <h1>Checkout</h1>
             {cartDetailsFromContext.checkoutArr.map(eachItem => <CheckoutItem key={eachItem.id} eachItem={eachItem}/>)}
             <div>
-                <p className='grandtotal'>Grand Total:{}</p>
+            <p className='grandtotal'>Grand Total: ${grandTotal.toFixed(2)}</p>
                 <button type='button' className='buynow-button'>Buy Now</button>
             </div>
         </div>
